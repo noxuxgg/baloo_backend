7ab73692228b1c20ebd201c7fe17e298b912c531
noxuxgg <fernandohuanca2005@gmail.com>
2026-04-26 13:08:46 -0400

    Agregando swagger y validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // habilitando (validacones) con class-validation
+  app.useGlobalPipes(new ValidationPipe())
+
+  // habilitando (DOCS API) con swagger
+
+  const config = new DocumentBuilder()
+  .setTitle("Proyecto BACKEND BALOO")
+  .setDescription("Backend para la aplicación de repostería Baloo")
+  .setVersion("1.0")
+  .addTag("Backend Nest")
+  .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
