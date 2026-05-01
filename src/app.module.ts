5f19b76fa99e5c221abb9f2cc47fc54b0e399adf
noxuxgg <fernandohuanca2005@gmail.com>
2026-05-01 10:18:01 -0400

    Conexion TYPEORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule],
+  imports: [ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5436,
+      username: 'postgres',
+      password: 'postgresql',
+      database: 'bdBaloo',
+      entities: [
+        __dirname + '/../**/*.entity{.ts,.js}'
+      ],
+      synchronize: false
+    }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
