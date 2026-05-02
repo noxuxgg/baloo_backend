b6c704a97d5d4f2377e93144fd8734cd5c1c1f63
noxuxgg <fernandohuanca2005@gmail.com>
2026-05-02 09:32:33 -0400

    bcrypt

--- src/modules/admin/usuarios/dto/create-usuario.dto.ts
+++ src/modules/admin/usuarios/dto/create-usuario.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUsuarioDto {}
+import { IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateUsuarioDto {
+
+    @IsString()
+    @MinLength(4)
+    @MaxLength(30)
+    nombreUsuario: string;
+
+    @IsString()
+    @MinLength(5)
+    @MaxLength(200)
+    contrasenia: string;
+
+}
