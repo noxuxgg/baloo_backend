27a55149da4fe85bcdb829864ba05c1435dbbba0
fatalbru <fatalbru@gmail.com>
2026-05-03 10:51:48 -0400

    Archivos dto pertenecientes a Henry

--- src/modules/admin/encargos/pagos-pedido/dto/create-pagos-pedido.dto.ts
+++ src/modules/admin/encargos/pagos-pedido/dto/create-pagos-pedido.dto.ts
@@ -1,1 +1,24 @@
-export class CreatePagosPedidoDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsDateString, IsNotEmpty, IsNumber, IsOptional, IsString, MinLength } from "class-validator";
+
+export class CreatePagosPedidoDto {
+    @ApiProperty({ description: 'Id que proviene de Pedido' })
+    @IsNumber()
+    @IsNotEmpty()
+    pedidoId: number;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    metodo: string;
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    monto: number;
+
+    @ApiProperty()
+    @IsDateString()
+    @IsNotEmpty()
+    fecha: Date;  
+}
--- src/modules/admin/encargos/pedidos/dto/create-pedido.dto.ts
+++ src/modules/admin/encargos/pedidos/dto/create-pedido.dto.ts
@@ -1,1 +1,69 @@
-export class CreatePedidoDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsDateString, IsNotEmpty, IsNumber, IsOptional, IsString, MinLength } from "class-validator";
+
+export class CreatePedidoDto {
+    @ApiProperty({ description: 'Id que proviene de Cliente' })
+    @IsNumber()
+    @IsNotEmpty()
+    clienteId: number;
+
+    @ApiProperty({ description: 'Id que proviene de Usuario'})
+    @IsNumber()
+    @IsNotEmpty()
+    usuarioId: number;
+
+    @ApiProperty({ description: 'Id que proviene de Sucursal'})
+    @IsNumber()
+    @IsNotEmpty()
+    sucursalId: number;
+
+    @ApiProperty()
+    @IsDateString()
+    @IsNotEmpty()
+    fechaPedido: Date;
+
+    @ApiProperty()
+    @IsDateString()
+    @IsNotEmpty()
+    fechaEntrega: Date;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    horaEntrega: string;
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    cantidadPersonas: number;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    estado: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8)
+    lugarEntrega: string;
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    total: number;
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    adelanto: number;
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    saldo: number;
+
+    @ApiProperty()
+    @IsString()
+    observaciones: string;
+}
