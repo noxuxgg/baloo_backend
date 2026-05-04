b95b4b268c999d17c922b6689dab4eb076ff6986
noxuxgg <fernandohuanca2005@gmail.com>
2026-05-03 21:25:04 -0400

    habilitando front

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   // habilitando (validacones) con class-validation
